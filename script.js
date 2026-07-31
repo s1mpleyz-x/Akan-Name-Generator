0ca7b1b1291889da425649af0984eaa3eaf803f3
Sammy Kariuki <simpleyzrx@gmail.com>
2026-07-31 08:57:46 +0300

    script(modified):added methods to fetch data from the form

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const akanForm = document.getElementById("generator-form");
 
 akanForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Stops the page from reloading
+
+    const formData = new FormData(akanForm); // Fetches data from the form
+
+    const day = formData.get('day');
+    const month = formData.get('month');
+    const year = formData.get('year');
+    const gender = formData.get('gender');
+
+
 })
